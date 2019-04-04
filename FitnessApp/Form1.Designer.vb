b9593bc562003290dec243f9d09200007d4021d3
Eddie Switek <etsdistribution@gmail.com>
2019-04-04 00:20:30 -0400

    Add files via upload

---
+++ FitnessApp/Form1.Designer.vb
@@ -0,0 +1,186 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FitnessApp
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.firstName = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.birthDate = New System.Windows.Forms.DateTimePicker()
+        Me.today = New System.Windows.Forms.DateTimePicker()
+        Me.lblResults = New System.Windows.Forms.Label()
+        Me.label5 = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'firstName
+        '
+        Me.firstName.Location = New System.Drawing.Point(274, 47)
+        Me.firstName.Name = "firstName"
+        Me.firstName.Size = New System.Drawing.Size(84, 20)
+        Me.firstName.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(4, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(654, 25)
+        Me.Label1.TabIndex = 222
+        Me.Label1.Text = "Let's See How Many Hours You've Exercised In Your Lifetime!"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(269, 70)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(108, 13)
+        Me.Label2.TabIndex = 22
+        Me.Label2.Text = "Enter your First Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(271, 132)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(105, 13)
+        Me.Label3.TabIndex = 22
+        Me.Label3.Text = "Enter your Birth Date"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(280, 186)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(67, 13)
+        Me.Label4.TabIndex = 622
+        Me.Label4.Text = "Current Date"
+        '
+        'birthDate
+        '
+        Me.birthDate.Location = New System.Drawing.Point(223, 109)
+        Me.birthDate.Name = "birthDate"
+        Me.birthDate.Size = New System.Drawing.Size(200, 20)
+        Me.birthDate.TabIndex = 1
+        '
+        'today
+        '
+        Me.today.Location = New System.Drawing.Point(223, 163)
+        Me.today.Name = "today"
+        Me.today.Size = New System.Drawing.Size(200, 20)
+        Me.today.TabIndex = 2
+        '
+        'lblResults
+        '
+        Me.lblResults.AutoSize = True
+        Me.lblResults.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResults.Location = New System.Drawing.Point(270, 257)
+        Me.lblResults.Name = "lblResults"
+        Me.lblResults.Size = New System.Drawing.Size(89, 19)
+        Me.lblResults.TabIndex = 222
+        Me.lblResults.Text = "lblResults"
+        Me.lblResults.Visible = False
+        '
+        'label5
+        '
+        Me.label5.AutoSize = True
+        Me.label5.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label5.Location = New System.Drawing.Point(173, 227)
+        Me.label5.Name = "label5"
+        Me.label5.Size = New System.Drawing.Size(327, 19)
+        Me.label5.TabIndex = 22
+        Me.label5.Text = "Congratulations! You've excersized a total of:"
+        Me.label5.Visible = False
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(262, 300)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(114, 45)
+        Me.btnCalculate.TabIndex = 3
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(262, 402)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(114, 45)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(262, 351)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(114, 45)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'FitnessApp
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(661, 465)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.label5)
+        Me.Controls.Add(Me.lblResults)
+        Me.Controls.Add(Me.today)
+        Me.Controls.Add(Me.birthDate)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.firstName)
+        Me.Name = "FitnessApp"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Fitness App"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents firstName As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents birthDate As DateTimePicker
+    Friend WithEvents today As DateTimePicker
+    Friend WithEvents lblResults As Label
+    Friend WithEvents label5 As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnClear As Button
+End Class
---
+++ FitnessApp/Form1.vb
@@ -0,0 +1,54 @@
+﻿'Eddie Switek
+'10-3-3018
+'CST 171 Fall 2018
+'Chapter 4 Exercise
+
+'This program calculates how much you've exercised in your entire lifetime
+'assuming you have exercised on an average of 2.5 hours a week.
+
+Public Class FitnessApp
+    Private Sub FitnessApp_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        'This will calculate the difference in weeks between the User's birthday and today.
+        Dim offset = New Date(1, 1, 1)
+        Dim dateOne = birthDate.Value
+        Dim dateTwo = today.Value
+        Dim diff As TimeSpan = dateTwo - dateOne
+        Dim years = (offset + diff).Year - 1
+        Dim months = (dateTwo.Month - dateOne.Month) '+ 12 * (dateTwo.Year - dateOne.Year)
+        Dim weeks = (years * 52) + (months * 4)
+
+        'This calculates how many hours the User has excersized in their lifetime.
+        Dim numHoursExercised As Integer = weeks * 2.5
+
+        'Display results on screen.
+        lblResults.Text = numHoursExercised.ToString & " hours!"
+        lblResults.Show()
+        label5.Show()
+
+
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        'This lets the User exit the program.
+        Me.Close()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        'This allows to the User to clear and reset all forms.
+        birthDate.ResetText()
+        today.ResetText()
+        firstName.Clear()
+        lblResults.Hide()
+        label5.Hide()
+
+    End Sub
+End Class
